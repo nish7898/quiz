0fda4320011c0ae6ea0b0dd14fac5f625ad0ed96
Nishant Kumar <nishant.adpr@gmail.com>
2018-12-07 22:18:06 +0530

    Changed the database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://nishant:bully1998@ds139909.mlab.com:39909/thebookbarter' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://nishant:bully1998@ds127944.mlab.com:27944/quiz-website' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
